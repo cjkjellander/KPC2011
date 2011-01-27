845a5a71f575470192b973e923b22af6ec7272cc
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2011-01-27 13:06:13 +0100

    updated game record

--- include/reversi.hrl
+++ include/reversi.hrl
@@ -2,14 +2,16 @@
 -define(REVERSI_HRL, 1).
 
 -record(game, {  id
-               , cookie_1
-               , cookie_2
+               , player_b
+               , player_w
                , board = {16#0000000810000000,
                           16#0000001008000000}
                , togo = 0
                , turns = 0
                , moves = []
                , points = {2, 2}
+               , start_time
+               , end_time
                }).
 
 -endif.
