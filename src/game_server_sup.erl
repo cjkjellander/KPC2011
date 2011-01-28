622da8aa51fa3c81e196cd4c99f583ff85aa2a8c
Markus Flambard <markus.flambard@klarna.com>
2011-01-28 07:01:15 +0100

    Added game server supervisor.

---
+++ src/game_server_sup.erl
@@ -0,0 +1,37 @@
+-module(game_server_sup).
+-behavior(supervisor).
+
+%% API
+-export([
+         start_link/0,
+         start_game_server/2
+        ]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+
+%%% API
+
+start_link() ->
+    supervisor:start_link({local, reversi_game_server_supervisor}, ?MODULE, []).
+
+start_game_server(GameNumber, Lobby) ->
+    supervisor:start_child(reversi_game_server_supervisor,
+                           [{GameNumber, Lobby}]).
+
+
+%%% Supervisor callbacks
+
+init(_Args) ->
+    {ok,
+     {
+       {simple_one_for_one, 5, 2000},
+       [{game_server,
+         {game_server, start_link, []},
+         transient,
+         1000,
+         worker,
+         [game_server]
+        }]
+     }}.
