115a4ab2e26cffbea24c3fdc71d2491c20fbe76b
Andreas Amsenius <andreas.amsenius@klarna.com>
2011-01-28 18:31:10 +0100

    games_resource generates json, does it work?

--- src/games_resource.erl
+++ src/games_resource.erl
@@ -1,31 +1,30 @@
 -module(games_resource).
 -export([init/1, content_types_provided/2,
-         to_html/2, to_json/2]).
+         to_json/2]).
 
 -include_lib("webmachine/include/webmachine.hrl").
 
 init([]) -> {ok, undefined}.
 
 content_types_provided(ReqData, State) ->
-    Types = [{"text/html", to_html}, {"application/json", to_json}],
+    Types = [{"application/json", to_json}],
     {Types, ReqData, State}.
-
-to_html(ReqData, State) ->
-    Games = lobby:client_command({list_games}),
-    {io_lib:format("~p", [Games]), ReqData, State}.
 
 to_json(ReqData, State) ->
     %% missing hostname!
     Path = wrq:path(ReqData),
+    Games = lobby:client_command({list_games}),
     GamesJson =
-        [ {"game", {struct, [{"rel", "self"},
-                             {"href", io_lib:format("~s/~s", [Path, Id]) }]}}
-          || Id <- lobby:client_command({list_games})],
+        [ {"game", {struct, [{"rel", <<"self">>},
+                             {"href", list_to_binary(
+                                        io_lib:format("~s/~w", [Path, Id])) }]}}
+          || Id <- Games],
     Json =
         {struct,
          [{"games",
-           [{"link", {struct, [{"rel", "self"},
-                               {"href", Path}]}}
-            |GamesJson]
+           {struct,
+            [{"link", {struct, [{"rel", <<"self">>},
+                                {"href", list_to_binary(Path)}]}}
+            |GamesJson]}
           }]},
     {mochijson2:encode(Json), ReqData, State}.
