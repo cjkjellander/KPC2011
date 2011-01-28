a845b922069b26987bccebe3e84dd4be26faf979
Carl-Johan Kjellander <carl-johan@klarna.com>
2011-01-28 20:55:53 +0100

    handle start of game. call both clients. {} around replies.

--- src/lobby.erl
+++ src/lobby.erl
@@ -81,7 +81,7 @@ handle_client_command({{login, User, Passwd}, IP}, From, #lobby_state{players = 
     check_inputs,
     case rev_bot:login(User, Passwd, IP) of
         {ok, _} ->
-            {reply, welcome, LS#lobby_state{players = [From | Ps]}};
+            {reply, {ok, welcome}, LS#lobby_state{players = [From | Ps]}};
         Error   ->
             {reply, Error, LS}
     end;
@@ -95,22 +95,28 @@ handle_client_command({{register, User, Player, Desc, Email}, IP},
     check_inputs,
     case rev_bot:register(User, Player, Desc, Email, IP, []) of
         {ok, PW} ->
-            {reply, {ok, password, PW}, LS#lobby_state{players = [From | Ps]}};
+            {reply, {ok, {password, PW}}, LS#lobby_state{players = [From | Ps]}};
         Error    ->
             {reply, Error, LS}
     end;
 
-handle_client_command({{i_want_to_play}, _IP}, From, #lobby_state{ready = RPs, games = Gs} = LS) ->
+handle_client_command({{i_want_to_play}, _IP}, {From, _}, #lobby_state{ready = RPs, games = Gs} = LS) ->
     NewLS =
         case RPs of
             [OtherPlayer] ->
                 {ok, #game{id = GameID}} = rev_game_db:new_game(),
-                {ok, GameServer} = game_server_sup:start_game_server(GameID, self()),
-                LS#lobby_state{ready = [], games = [{GameID, GameServer} | Gs]};
+                {ok, GameServer} =
+                    game_server_sup:start_game_server(GameID, self()),
+                NewLS = LS#lobby_state{ready = [],
+                                       games = [{GameID, GameServer} | Gs]},
+                gen_server:cast(OtherPlayer,
+                                {redirect, {lets_play, GameServer, GameID}}),
+                {reply, {redirect, {lets_play, GameServer, GameID}}, NewLS};
             [] ->
-                LS#lobby_state{ready = [From]}
-        end,
-    {reply, get_ready_for_some_action, NewLS};
+                NewLS = LS#lobby_state{ready = [From]},
+                {reply, {ok, waiting_for_challenge}, NewLS}
+        end;
+
 
 handle_client_command({{list_games}, _IP}, _From, #lobby_state{games = Games} = LS) ->
     {reply, {ok, [Id || {Id, GameServer} <- Games]}, LS};
