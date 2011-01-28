0fc18cdcfc57f8622fdc067693cf80540c2a75ee
Carl-Johan Kjellander <carl-johan@klarna.com>
2011-01-28 16:20:08 +0100

    handle registration

--- src/rev_bot.erl
+++ src/rev_bot.erl
@@ -13,6 +13,10 @@
 -export([ create_table/0
         , read/1
         , write/1
+        , register/6
+        , gen_passwd/0
+        , gen_hash/2
+        , check_passwd/2
         ]).
 
 -define(SERVER, ?MODULE).
@@ -42,3 +46,40 @@ write(#rev_bot{} = Bot) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+register(Name
+         , Player
+         , Desc
+         , Email
+         , LastIP
+         , Extra
+        ) when is_list(Name)   andalso Name   =/= [] andalso
+               is_list(Player) andalso Player =/= [] andalso
+               is_list(Desc)   andalso
+               is_list(Email)  andalso Email  =/= [] andalso
+               is_list(Extra)  ->
+    {PW, Hash} = gen_passwd(),
+    write(#rev_bot{name=Name
+                   , player=Player
+                   , password=Hash
+                   , descr=Desc
+                   , email=Email
+                   , last_ip=LastIP
+                   , extra=Extra
+                  }),
+    {ok, PW}.
+
+gen_passwd() ->
+    TempStr = crypto:rand_bytes(16),
+    Salt = base64:encode_to_string(crypto:rand_bytes(16)),
+    PassWord = lists:sublist(base64:encode_to_string(TempStr), 10),
+    Hash = gen_hash(Salt, PassWord),
+    {PassWord, Hash}.
+
+gen_hash(Salt, Password) ->
+    Hash = crypto:md5(Salt++Password),
+    {Salt, Hash}.
+
+check_passwd({Salt, Hash}, Password) ->
+    Hash2 = gen_hash(Salt, Password),
+    Hash =:= Hash2.
