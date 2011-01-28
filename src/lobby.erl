c3a4dff308844340738b9276831cec231215cfbe
Markus Flambard <markus.flambard@klarna.com>
2011-01-28 11:13:59 +0100

    The lobby server is alive!

--- src/lobby.erl
+++ src/lobby.erl
@@ -1,11 +1,105 @@
 -module(lobby).
 -behavior(gen_server).
--export([start_link/0
-         , stop/0
-         ]).
+
+-include("../include/reversi.hrl").
+
+%% API
+-export([
+         start_link/0,
+         client_command/1
+        ]).
+
+%% gen_server callbacks
+-export([
+         init/1,
+         code_change/3,
+         handle_call/3,
+         handle_cast/2,
+         handle_info/2,
+         terminate/2
+        ]).
+
+-record(lobby_state,
+        {
+          players = [],
+          ready_for_play = [],
+          games = []
+        }).
+
+-record(player,
+        {
+          name,
+          pid
+        }).
+
+
+%%% API
 
 start_link() ->
+    gen_server:start_link({local, reversi_lobby}, ?MODULE, [], []).
+
+%% This functions is called by the client handler.
+client_command(Command) ->
+    gen_server:call(reversi_lobby, {cmd, Command}).
+
+%% enter(Name) ->
+%%     gen_server:call(reversi_lobby, {enter, Name}).
+
+
+%%% gen_server callbacks
+
+init(_Args) ->
+    {ok, #lobby_state{}}.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+handle_call({cmd, Command}, From, State) ->
+    handle_client_command(Command, From, State);
+handle_call(_Request, _From, State) ->
+    {noreply, State}.
+
+handle_cast(_Request, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
     ok.
 
-stop() ->
-    ok.
+
+%%% Internal functions
+
+handle_client_command({game, GameID, Command}, From, LS) ->
+    %% Locate the game server with GameID.
+    handle_client_game_command(GameID, From, Command, LS);
+
+handle_client_command({login, User, Passwd}, From, #lobby_state{players = Ps} = LS) ->
+    do_login_stuff,
+    {reply, welcome, LS#lobby_state{players = [From | Ps]}};
+
+handle_client_command({logout}, From, #lobby_state{players = Ps, ready_for_play = RPs} = LS) ->
+    {reply, good_bye, LS#lobby_state{players = lists:remove(From, Ps),
+                                     ready_for_play = lists:remove(From, RPs)}};
+
+handle_client_command({register, User, Passwd}, From, #lobby_state{players = Ps} = LS) ->
+    do_register_stuff,
+    {reply, welcome, LS#lobby_state{players = [From | Ps]}};
+
+handle_client_command({i_want_to_play}, From, #lobby_state{ready_for_play = RPs} = LS) ->
+    Ready =
+        case RPs of
+            [OtherPlayer] ->
+                #game{id = GameID} = rev_game_db:new_game(),
+                game_server_sup:start_game_server(GameID, self()),
+                [];
+            [] ->
+                [From]
+        end,
+    {reply, get_ready_for_some_action, LS#lobby_state{ready_for_play = Ready}}.
+
+
+
+handle_client_game_command(_GameServer, From, _Command, LS) ->
+    {reply, {error, unknown_game_command}, LS}.
